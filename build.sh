87b4faa3fab7b25cb94485a74cc2d53ac3d1a76c
deigote <diego@toharia.com>
2012-10-30 21:50:09 +0100

    There is no need to rebuild the pdf

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ git remote update
 if [[ `git status -uno | grep "# Your branch is behind"` ]] ; then 
         echo "We are behind, building a new release..."
         git pull origin master
-	pdflatex Diego\ Toharia\ -\ CV\ \(English\).tex        
         curl -X PURGE http://deigote.com/cv &> /dev/null
         curl -X PURGE http://deigote.com/cv/Diego\ Toharia\ -\ CV\ \(English\).pdf &> /dev/null
 else 
