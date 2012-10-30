f92dda709bf0a6f7d0d300e59941815c91d9c62a
deigote <diego@toharia.com>
2012-10-30 22:23:10 +0100

    Modified purgation

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ if [[ `git status -uno | grep "# Your branch is behind"` ]] ; then
         echo "We are behind, building a new release..."
         git pull origin master
         curl -X PURGE http://deigote.com/cv &> /dev/null
-        curl -X PURGE http://deigote.com/cv/Diego\ Toharia\ -\ CV\ \(English\).pdf &> /dev/null
+	curl -X PURGE "http://deigote.com/cv/Diego%20Toharia%20-%20CV%20(English).pdf"       
 else 
         echo "Everything is up to date"
 fi
