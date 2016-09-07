6a24702749d3d39286fc599f3c67ca8da76e138a
deigote <diego@toharia.com>
2016-09-07 20:25:01 +0200

    Build process is adapted to multiple source files

--- build.sh
+++ build.sh
@@ -7,18 +7,20 @@ if [ ! -z "$1" ] ; then
 	OUTPUT_DIR="$1"
 	mkdir -p $OUTPUT_DIR
 fi
-OUTPUT_FILE=Diego\ Toharia\ -\ CV\ \(English\).pdf
-echo "$OUTPUT_DIR/$OUTPUT_FILE"
 
-git remote update
-if [[ ! -f "$OUTPUT_DIR/$OUTPUT_FILE" || `git status -uno | grep "Your branch is behind"` ]] ; then 
-        echo "We are behind, building a new release..."
-        git pull origin master
-	docker build -t cv-updater .
-	docker run --rm -v "$(pwd)":/cv-src -v "$OUTPUT_DIR":/cv-out -i -t cv-updater bash -c \
-		"cd /cv-src && pdflatex Diego\ Toharia\ -\ CV\ \(English\).tex && mv '$OUTPUT_FILE' /cv-out"
-        curl -X PURGE http://deigote.com/cv &> /dev/null
-	curl -X PURGE "http://deigote.com/cv/Diego%20Toharia%20-%20CV%20(English).pdf" &> /dev/null
-else 
-        echo "Everything is up to date"
-fi
+IFS=$'\n'
+for source in *.tex ; do
+	OUTPUT_FILE="$(basename $source '.tex').pdf"
+	OUTPUT="$OUTPUT_DIR$OUTPUT_FILE"
+	if [[ ! -f "$OUTPUT" || `git status -uno | grep "Your branch is behind"` ]] ; then 
+	        echo "We are behind, building a new release..."
+        	git pull origin master
+		docker build -t cv-updater .
+		docker run --rm -v "$(pwd)":/cv-src -v "$OUTPUT_DIR":/cv-out -i -t cv-updater bash -c \
+			"cd /cv-src && pdflatex '$source' && mv '$OUTPUT_FILE' /cv-out"
+	        curl -X PURGE http://deigote.com/cv &> /dev/null
+		curl -X PURGE "http://deigote.com/cv/Diego%20Toharia%20-%20CV%20(English).pdf" &> /dev/null
+	else 
+        	echo "Everything is up to date"
+	fi
+done
